d6510ac86ad27dee6cacc5f3df3d39eb9aeaafd6
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-11-09 17:07:20 -0200

    Fix coins' names.

--- labels.py
+++ labels.py
@@ -7,20 +7,37 @@ from enum import Enum
 class Url(Enum):
     """Each URL represents a base PATH for API requests."""
 
-    API = 'https://www.mercadobitcoin.net/api'
+    HOST = 'https://www.mercadobitcoin.net'
+    API_PATH = '/api/'
+    TAPI_PATH = '/tapi/v3/'
+
+    API = HOST + API_PATH
+    TAPI = HOST + TAPI_PATH
 
 
 class Coin(Enum):
     """Each coin represents its value for API params."""
 
+    BRL = 'BRL'
     BTC = 'BTC'
-    LTC = 'LTC'
-    BCH = 'BCH'
+    LTC = 'BRLLTC'
+    BCH = 'BRLBCH'
 
 
 class Method(Enum):
-    """Each method represents its name for API params."""
+    """Each method represents its name for API and TAPI params."""
 
     TICKER = 'ticker'
     ORDERBOOK = 'orderbook'
     TRADES = 'trades'
+
+    SYS_MSG = 'list_system_messages'
+    ACCOUNT_INFO = 'get_account_info'
+    GET_ORDER = 'get_order'
+    ORDERS = 'list_orders'
+    LIST_ORDERBOOK = 'list_orderbook'
+    BUY = 'place_buy_order'
+    SELL = 'place_sell_order'
+    CANCEL = 'cancel_order'
+    WITHDRAWAL = 'get_withdrawal'
+    WITHDRAW = 'withdraw_coin'
