d565dc9a87779185e7e3dd7c18cda041c6df5ffd
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-11-13 15:38:58 -0200

    PIP package files

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Mercado Bitcoin's API Interface
 A Python 3 API interface for Mercado Bitcoin's API.
 
+# Instalation
+```bash
+sudo pip3 install mb-api
+```
+
 ## Note
 Still in development.
+
+UnicodeEncodeError could occur.
+To fix it, set the locale as:
+LANGUAGE=cs_CZ.UTF-8
+LC_ALL=cs_CZ.UTF-8
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='mbapi',
+    packages=['mbapi'],
+    version='0.1',
+    description='A Python 3 API interface for Mercado Bitcoin\'s API.',
+    author='Bernardo Trevizan',
+    author_email='trevizanbernardo@gmail.com',
+    url='https://github.com/btrevizan/mb_api'
+)
