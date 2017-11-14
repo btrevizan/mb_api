064a207758782e02d10cacbfa712a714b0ef2c09
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-11-14 11:39:03 -0200

    Setup with new version

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='mbapi',
     packages=['mbapi'],
-    version='0.1',
+    version='0.1.2',
     description='A Python 3 API interface for Mercado Bitcoin\'s API.',
     author='Bernardo Trevizan',
     author_email='trevizanbernardo@gmail.com',
-    url='https://github.com/btrevizan/mb_api'
+    url='https://github.com/btrevizan/mb_api',
+    download_url='https://github.com/btrevizan/mb_api/dist/mbapi-0.1.tar.gz',
+    keywords='bitcoin,api,mercado_bitcoin'
 )
